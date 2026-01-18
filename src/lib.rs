4b485dcb46173330d776bc15076d3210ded32b44
Landon <landon.creel@protonmail.com>
2026-01-17 17:12:25 -0800

    Adding Case Insensitive Test Case

--- src/lib.rs
+++ src/lib.rs
@@ -13,10 +13,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
-        let contents = "Rust:\nsafe, fast, productive.\nPick three.";
+        let contents = "Rust:\nsafe, fast, productive.\nPick three.\nDuct tape.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "Rust:\nsafe, fast, productive.\nPick three.\nTrust me.";
+
+        assert_eq!(vec!["Rust:", "Trust me."], search(query, contents));
     }
 }
